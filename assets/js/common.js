13d2cc6382c24c1ec48fcf7322e4872458467177
cubickco <work.pasternak@gmail.com>
2019-08-19 21:33:08 +0300

    oh, I do this. First deploy this project

---
+++ assets/js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$("#portfolio_grid").mixItUp();
+
+	$(".s_portfolio li").click(function() {
+		$(".s_portfolio li").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$(".popup").magnificPopup({type:"image"});
+	$(".popup_content").magnificPopup({
+		type:"inline",
+		midClick: true
+	});
+
+	$(".portfolio_item").each(function(i) {
+		$(this).find("a").attr("href", "#work_" + i);
+		$(this).find(".podrt_descr").attr("id", "work_" + i);
+	});
+
+});
